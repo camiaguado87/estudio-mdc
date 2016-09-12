66b4b4b5631fbc93c583a10283b10147d4c4b8e1
Juan Cruz Leyba <jcleyba@MacBook-Air-de-Juan.local>
2016-09-12 20:42:13 -0300

    Fixed gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,68 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache');
+var minifycss = require('gulp-minify-css');
 var less = require('gulp-less');
-var watch = require('gulp-watch');
-var cleanCSS = require('gulp-clean-css');
-var livereload = require('gulp-livereload');
-var sourcemaps = require('gulp-sourcemaps');
-var notify = require("gulp-notify");
+var browserSync = require('browser-sync');
 
-gulp.task('less', function () {
-    gulp.src('./app/less/style.less')
-        .pipe(sourcemaps.init())
-        .pipe(less())
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./app/css'))
-        .pipe(notify('Success!!'))
-        .pipe(livereload());
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+       baseDir: "./"
+    }
+  });
 });
 
-gulp.task('watch', function () {
-    livereload.listen(4100);
-    gulp.watch('./app/less/*.less', ['less']);
+gulp.task('bs-reload', function () {
+  browserSync.reload();
 });
+
+gulp.task('images', function(){
+  gulp.src('./src/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('./dist/images/'));
+});
+
+gulp.task('styles', function(){
+  gulp.src(['src/styles/less/*.less'])
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(less())
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('./src/js/*.js')
+    .pipe(plumber({
+      errorHandler: function (error) {
+        console.log(error.message);
+        this.emit('end');
+    }}))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('default', ['browser-sync'], function(){
+  gulp.watch("./src/styles/**/*.less", ['styles']);
+  gulp.watch("./src/scripts/**/*.js", ['scripts']);
+  gulp.watch("*.html", ['bs-reload']);
+});
+
+gulp.task('build',['images','styles','scripts']);
