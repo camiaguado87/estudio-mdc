7c13af7e18de7ddf72ba08a3755651f10d4b406a
juancruz <juancruz@MacBook-Pro-de-juancruz.local>
2017-03-13 21:32:24 -0300

    added images

--- gulpfile.js
+++ gulpfile.js
@@ -13,59 +13,61 @@ var watch = require('gulp-watch');
 var livereload = require('gulp-livereload');
 
 
-gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-       baseDir: "./"
-    }
-  });
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
 
 gulp.task('bs-reload', function () {
-  browserSync.reload();
+    browserSync.reload();
 });
 
-gulp.task('images', function(){
-  gulp.src('./src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('./dist/images/'));
+gulp.task('images', function () {
+    gulp.src('./src/images/**/*')
+        .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
+        .pipe(gulp.dest('./dist/images/'));
 });
 
-gulp.task('styles', function(){
-  gulp.src(['src/styles/less/*.less'])
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(less())
-    .pipe(autoprefixer('last 2 versions'))
-    .pipe(gulp.dest('./dist/css/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(minifycss())
-    .pipe(gulp.dest('./dist/css/'))
-    .pipe(browserSync.reload({stream:true}))
+gulp.task('styles', function () {
+    gulp.src(['src/styles/less/*.less'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(less())
+        .pipe(autoprefixer('last 2 versions'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('scripts', function(){
-  return gulp.src('./src/js/*.js')
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
-    }}))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('./dist/js/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js/'))
-    .pipe(browserSync.reload({stream:true}))
+gulp.task('scripts', function () {
+    return gulp.src('./src/js/*.js')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js/'))
+        .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('default', ['browser-sync'], function(){
-  gulp.watch("./src/styles/**/*.less", ['styles']);
-  gulp.watch("./src/scripts/**/*.js", ['scripts']);
-  gulp.watch("*.html", ['bs-reload']);
+gulp.task('default', ['browser-sync'], function () {
+    gulp.watch("./src/styles/**/*.less", ['styles']);
+    gulp.watch("./src/scripts/**/*.js", ['scripts']);
+    gulp.watch("*.html", ['bs-reload']);
 });
 
 gulp.task('watch', function () {
@@ -73,4 +75,4 @@ gulp.task('watch', function () {
     gulp.watch('./src/styles/less/*.less', ['styles']);
 });
 
-gulp.task('build',['images','styles','scripts']);
+gulp.task('build', ['images', 'styles', 'scripts']);
