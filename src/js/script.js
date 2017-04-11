f53d0c6124422b34a5c0e0be4531a5fc1fb8f02c
Juan Cruz Leyba <juancruz.leyba@santexgroup.com>
2017-04-10 22:21:12 -0300

    adding modals

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,62 @@
 $(document).ready(function () {
-    var $window = $(window),
-        scroll = 0,
-        $nav = $("header nav")
-    $window.scroll(function (e) {
-        var savedScroll = $(document).scrollTop();
-        if (savedScroll < 1) {
-            $nav.addClass('not-scrolled');
-            $nav.removeClass('scrolled');
+    var $window = $(window);
+    //     scroll = 0,
+    //     $nav = $("header nav");
+    // $window.scroll(function (e) {
+    //     var savedScroll = $(document).scrollTop();
+    //     if (savedScroll < 1) {
+    //         $nav.addClass('not-scrolled');
+    //         $nav.removeClass('scrolled');
+    //
+    //     } else if (savedScroll < scroll) {
+    //         $nav.addClass('scrolled');
+    //         $nav.removeClass('not-scrolled');
+    //     } else if (savedScroll > scroll) {
+    //         $nav.removeClass('scrolled');
+    //     }
+    //     scroll = savedScroll;
+    // });
 
-        } else if (savedScroll < scroll) {
-            $nav.addClass('scrolled');
-            $nav.removeClass('not-scrolled');
-        } else if (savedScroll > scroll) {
-            $nav.removeClass('scrolled');
+    var headerHeight = $('header').height(),
+        $nav = $("header nav"),
+        $header = $('header'),
+        offset = 0;
+    $window.scroll(function (e) {
+        offset = $window.scrollTop();
+        if (window.innerWidth < 768) {
+            $header.css('background', 'white');
+            $('#areas .hero-bg').height($('#areas').height());
+            if (offset > headerHeight) {
+                $header.addClass('scrolled');
+            }
+            else {
+                $header.removeClass('scrolled');
+            }
         }
-        scroll = savedScroll;
-    })
+        else {
+            if (offset > headerHeight) {
+                $nav.addClass('scrolled');
+            }
+            else {
+                $nav.removeClass('scrolled');
+            }
+        }
+    });
 
     $('nav ul li a').click(function (e) {
         e.preventDefault();
         var id = $(this).attr("href");
         var target = $(id.toString());
-        $(window).scrollTo(target, 800);
+        if (window.innerWidth < 768) {
+            $(window).scrollTo(target, 800, {offset: -112});
+            $nav.hide();
+        }
+        else {
+            $(window).scrollTo(target, 800, {offset: -62});
+        }
+    });
+
+    $('.mobile.nav').click(function (e) {
+        $nav.slideToggle();
     })
 });
